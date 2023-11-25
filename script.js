db83b5950876c6c6ec4b09e1ad72a084c62ba016
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-11-26 04:41:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let ptag = document.querySelector('#counter');
+let number = Number(ptag.innerText);
+
+let btn = document.querySelector('button');
+console.log(btn);
+btn.addEventListener('click',()={
+	number ++;
+	ptag.innerText= `${number}`
+})
